503c4a6bd67020c1fa6520b693fd2faf263c0559
davileahy <davileahy2@gmail.com>
2024-10-03 13:08:22 -0300

    Better project description on MorePage. Further changes comming soon.

--- src/pages/MorePage.js
+++ src/pages/MorePage.js
@@ -19,7 +19,9 @@ const MorePage = () => {
             <Box bg="white" p={6} borderRadius="md" boxShadow="lg">
             <Heading as="h2" size="lg" mb={4} color="#2980B9">O Projeto</Heading>
             <Text fontSize="lg" color="#34495E">
-                Este projeto tem como objetivo proporcionar acesso a plataformas gratuitas para o aprendizado de idiomas, especialmente o inglês. Nossa missão é democratizar o aprendizado e facilitar a educação de qualidade para todos.
+                O <strong>Comunicação Sem Fronteiras</strong> surgiu como um projeto de extensão da nossa universidade. No entanto, agora é muito mais do que isso. <br /> 
+                Servindo como um hub de plataformas e e-books, o acesso á <strong>educação</strong> e o <strong>conhecimento</strong> está aqui, livre para todos. <br />
+                Temos como objetivo proporcionar acesso a plataformas gratuitas para o aprendizado. Nossa missão é democratizar o aprendizado e facilitar a educação de qualidade para todos.
             </Text>
             </Box>
 
@@ -33,7 +35,7 @@ const MorePage = () => {
             </Box>
 
             <Box bg="white" p={6} borderRadius="md" boxShadow="lg">
-            <Heading as="h2" size="lg" mb={4} color="#2980B9">Desenvolvimento</Heading>
+            <Heading as="h2" size="lg" mb={4} color="#2980B9">Desenvolvido por:</Heading>
             <Stack spacing={4}>
                 <Box display="flex" alignItems="center">
                 <Text fontSize="lg" color="#34495E" ml={4}><Icon as={BsCodeSquare}></Icon> Davi Leahy - Desenvolvimento Web <Button as={'a'} href='https://www.linkedin.com/in/davileahy'><BsLinkedin/></Button> </Text>
