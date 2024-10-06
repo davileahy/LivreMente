899f54eec8b9478f74e77b74f131c01709819588
davileahy <davileahy2@gmail.com>
2024-10-06 19:47:00 -0300

    Better navbar responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,34 @@
-import React from "react";
-import { Box, Flex, Button, Icon } from "@chakra-ui/react";
-import { BsJournalBookmarkFill, BsChatDots, BsBook } from "react-icons/bs";
+import React, { useState } from "react";
+import { Box, Flex, Button, Icon, useBreakpointValue, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, IconButton } from "@chakra-ui/react";
+import { BsJournalBookmarkFill, BsChatDots, BsBook, BsList } from "react-icons/bs";
 import { Link as ScrollLink } from "react-scroll";
 import small_logo from "../assets/favicon.png";
 
 const Navbar = () => {
+  // Estado para controlar a abertura do drawer (menu mobile)
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const onOpen = () => setIsOpen(true);
+
+  // Definir comportamento da logo e layout baseado nos breakpoints
+  const logoSize = useBreakpointValue({ base: "40px", md: "60px" });
+  const displayLinks = useBreakpointValue({ base: "none", md: "flex" });
+
   return (
-    <Box as="nav" bg="white" py={8} px={10} className="fixed w-full z-10">
+    <Box as="nav" bg="white" py={4} px={6} className="fixed w-full z-10 shadow-md">
       <Flex justify="space-between" align="center">
+        {/* Logo responsiva */}
         <ScrollLink to="inicio" smooth={true} duration={500} offset={-70}>
           <img
             src={small_logo}
             alt="logo"
-            className="h-auto max-w-16 hover:scale-125 duration-[0.5s] hover:cursor-pointer"
+            style={{ width: logoSize, height: "auto" }}
+            className="hover:scale-125 duration-[0.5s] hover:cursor-pointer"
           />
         </ScrollLink>
 
-        <Flex gap={10}>
+        {/* Links exibidos em telas maiores */}
+        <Flex gap={10} display={displayLinks}>
           <Button
             variant="link"
             color="#0D415D"
@@ -31,7 +43,6 @@ const Navbar = () => {
               color="#0D415D"
               size="lg"
               className="hover:scale-125 duration-[0.5s]"
-              href=""
             >
               Sobre <Icon as={BsChatDots} ml={2} />
             </Button>
@@ -47,6 +58,53 @@ const Navbar = () => {
             </Button>
           </ScrollLink>
         </Flex>
+
+        {/* Ícone do menu hamburguer para telas menores */}
+        <Box display={{ base: "block", md: "none" }}>
+          <IconButton
+            icon={<BsList />}
+            onClick={onOpen}
+            aria-label="Open Menu"
+            color="#0D415D"
+            fontSize="2xl"
+            variant="ghost"
+            _hover={{ bg: "transparent", transform: "scale(1.2)" }}
+          />
+        </Box>
+
+        {/* Drawer (menu lateral) para dispositivos móveis */}
+        <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Menu</DrawerHeader>
+
+            <DrawerBody>
+              <Flex direction="column" gap={4}>
+                <ScrollLink to="inicio" smooth={true} duration={500} offset={-70} onClick={onClose}>
+                  <Button variant="link" color="#0D415D" size="lg">
+                    Início
+                  </Button>
+                </ScrollLink>
+                <ScrollLink to="biblioteca" smooth={true} duration={500} offset={-70} onClick={onClose}>
+                  <Button variant="link" color="#0D415D" size="lg">
+                    Biblioteca <Icon as={BsBook} ml={2} />
+                  </Button>
+                </ScrollLink>
+                <ScrollLink to="sobre" smooth={true} duration={500} offset={-70} onClick={onClose}>
+                  <Button variant="link" color="#0D415D" size="lg">
+                    Sobre <Icon as={BsChatDots} ml={2} />
+                  </Button>
+                </ScrollLink>
+                <ScrollLink to="cursos" smooth={true} duration={500} offset={-70} onClick={onClose}>
+                  <Button variant="link" color="#0D415D" size="lg">
+                    Cursos <Icon as={BsJournalBookmarkFill} ml={2} />
+                  </Button>
+                </ScrollLink>
+              </Flex>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
       </Flex>
     </Box>
   );
